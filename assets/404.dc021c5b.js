9ab07cd26449ee8a9fbf013c103f95c06513b101
aqib7275 <aqib7275@users.noreply.github.com>
2022-07-16 08:37:11 +0000

    deploy: 1749672fe3587128ed84e42e1086609e5e8ea230

---
+++ assets/404.dc021c5b.js
@@ -0,0 +1,1 @@
+import"./index.7a8f65e8.js";import{B as o}from"./button.629e5c22.js";import{R as s}from"./index.3c646e98.js";import{k as t,m as e}from"./react-venders.44f35f05.js";const p=()=>t(s,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t(o,{type:"primary",onClick:()=>e.push("/"),children:"Back Home"})});export{p as default};
---
+++ assets/RootLayout.35c70b3e.js
@@ -0,0 +1,1 @@
+import{k as o,F as a}from"./react-venders.44f35f05.js";const s=({children:t})=>o(a,{children:t});export{s as default};
